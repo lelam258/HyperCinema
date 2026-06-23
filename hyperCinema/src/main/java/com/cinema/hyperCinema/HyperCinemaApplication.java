10edbbcdfc6c9ad254068a8cf34c4e94f1353b1e
HungTP <hungtphe182569@fpt.edu.vn>
2026-06-23 12:05:10 +0700

    Print clickable localhost URL in console on startup

--- hyperCinema/src/main/java/com/cinema/hyperCinema/HyperCinemaApplication.java
+++ hyperCinema/src/main/java/com/cinema/hyperCinema/HyperCinemaApplication.java
@@ -10,6 +10,10 @@ public class HyperCinemaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HyperCinemaApplication.class, args);
+		System.out.println("=================================================");
+		System.out.println("HyperCinema started successfully!");
+		System.out.println("Access local site: http://localhost:8080");
+		System.out.println("=================================================");
 	}
 
 }
