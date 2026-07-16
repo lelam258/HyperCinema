f3d69160b936d6b8942ca4f3c29929ce1a86a35c
lelam258 <lnldh258@gmail.com>
2026-07-16 11:04:38 +0700

    fix conflict ver 1

--- hyperCinema/src/main/java/com/cinema/hyperCinema/controller/admin/BranchController.java
+++ hyperCinema/src/main/java/com/cinema/hyperCinema/controller/admin/BranchController.java
@@ -312,19 +312,6 @@ public class BranchController {
         }
     }
 
-    @GetMapping("/{branchId}/staff/assign")
-    public String assignStaffForm(@PathVariable Integer branchId, Model model) {
-
-        BranchDetailView branch = branchService.findById(branchId);
-
-        model.addAttribute("branch", branch);
-        model.addAttribute("staffCandidates", branchService.listUnassignedStaff());
-        model.addAttribute("managerCandidates", branch.getManagers());
-        model.addAttribute("assignStaffRequest", new AssignStaffRequest());
-
-        return "admin/branches/assign-staff";
-    }
-
     @PostMapping("/{branchId}/staff/{userId}/unassign")
     public String unassignStaff(@PathVariable Integer branchId,
                                 @PathVariable Integer userId,
